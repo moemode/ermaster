3c8a292a59fe88bc45ad58ca215c6b6275f5f669
Moritz Pfeffer <st152880@stud.uni-stuttgart.de>
2023-10-28 17:44:01 +0200

    Adds batch invocation

--- gpt.py
+++ gpt.py
@@ -33,8 +33,8 @@ def write_json_iter(it, N, fh):
 
 def get_completions(prompts, targets, model_params):
     for p, t in zip(prompts, targets):
-        c = completions_with_backoff(prompt=p, **model_params)
-        yield {"p": p, "t": t, "c": c.to_dict_recursive()}
+        resp = completions_with_backoff(prompt=p, **model_params)
+        yield {"p": p, "t": t, "c": resp.choices[0].to_dict_recursive()}
 
 
 def num_tokens_from_string(string, model):
@@ -44,16 +44,50 @@ def num_tokens_from_string(string, model):
     return num_tokens
 
 
+def batch_get_completions(prompts, targets, model, model_params):
+    batch_prompts = []
+    batch_truths = []
+    batch_tokens = 0
+    for p, t in zip(prompts, targets):
+        pt = num_tokens_from_string(p, model)
+        if batch_tokens + pt + len(batch_prompts) * model_params["max_tokens"] < 3500:
+            batch_prompts.append(p)
+            batch_truths.append(t)
+            batch_tokens += pt
+        else:
+            r = completions_with_backoff(prompt=batch_prompts, **model_params)
+            for choice in r.choices:
+                yield {
+                    "p": batch_prompts[choice.index],
+                    "t": batch_truths[choice.index],
+                    "c": choice,
+                }
+            batch_prompts = [p]
+            batch_truths = [t]
+            batch_tokens = pt
+    if len(batch_prompts) > 0:
+        r = completions_with_backoff(prompt=batch_prompts, **model_params)
+        for choice in r.choices:
+            yield {
+                "p": batch_prompts[choice.index],
+                "t": batch_truths[choice.index],
+                "c": choice,
+            }
+
+
 dataset = "0_beer20.json"
 model_params = dict(model=model, max_tokens=1, logprobs=5, temperature=0)
 prompts, targets = prompts_targets(Path(f"data/{dataset}"))
 task = dataset
 completions = []
-run_path = Path(f"runs/{task}_{model}.json", "w")
+run_path = Path(f"runs/{task}_{model}.json")
 if run_path.exists():
     assert False
 
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 with open(f"runs/{task}_{model}.json", "w+") as f:
-    write_json_iter(get_completions(prompts[:6], targets[:6], model_params), 6, f)
+    write_json_iter(
+        batch_get_completions(prompts, targets, model, model_params), len(prompts), f
+    )
+    # write_json_iter(get_completions(prompts[:6], targets[:6], model_params), 6, f)
--- inspect_resp.py
+++ inspect_resp.py
@@ -1,8 +1,11 @@
 import pickle
+
 # Later, you can load the object back
-with open('my_object.pkl', 'rb') as file:
+with open("my_object.pkl", "rb") as file:
     c = pickle.load(file)
-    c.items()
-    print(c)
-    
-#from openai.openai_object import OpenAIObject
+
+with open("completions.pkl", "rb") as file:
+    cs = pickle.load(file)
+
+print(c)
+# from openai.openai_object import OpenAIObject
