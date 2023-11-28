c6e1d590827e391abe20cd80de3c007060b7048b
moemode <pfeffer.moritz@gmail.com>
2023-11-28 18:45:59 +0100

    Adds llm_perf plots

--- pre_llm_vis.py
+++ pre_llm_vis.py
@@ -2,6 +2,20 @@ import seaborn as sns
 import pandas as pd
 
 from utils import rename_datasets
+import matplotlib.pyplot as plt
+
+
+def setup_plt():
+    # Override matplotlib default styling.
+    plt.style.use("seaborn-v0_8")
+    plt.rc("font", size=12)
+    plt.rc("axes", labelsize=12)
+    plt.rc("xtick", labelsize=12)
+    plt.rc("ytick", labelsize=12)
+    plt.rc("legend", fontsize=12)
+
+    plt.rc("axes", titlesize=16)
+    plt.rc("figure", titlesize=16)
 
 
 def plot_dataset(data, dataset_name):
@@ -14,43 +28,43 @@ def plot_dataset(data, dataset_name):
         marker="o",
         data=data,
     )
-
+    g.set_axis_labels("Threshold", "Value")
+    g.fig.subplots_adjust(top=0.92)
     # Set title for the plot
     g.fig.suptitle(f"{dataset_name}")
-    g.savefig(f"figures/{dataset_name}-pre_llm_perf.png")
+    g.savefig(f"figures/pre_llm_perf-{dataset_name}.png")
 
 
-df = pd.read_csv("eval_writeup/pre_llm_perf.csv")
-id_vars = ["Dataset", "Threshold"]
-selected_metrics = ["Precision", "Recall", "F1"]
+if __name__ == "__main__":
+    setup_plt()
+    df = pd.read_csv("eval_writeup/pre_llm_perf.csv")
+    id_vars = ["Dataset", "Threshold"]
+    selected_metrics = ["Precision", "Recall", "F1"]
+    # Filter the dataframe to only contain the selected metrics and id_vars
+    df = df[selected_metrics + id_vars]
+    # Reshape the dataframe for seaborn
+    df_melted = pd.melt(df, id_vars=id_vars, var_name="Metric", value_name="Value")
+    df_melted = rename_datasets(df_melted, preserve_sampled=False)
+    # Create the relplot
+    g = sns.relplot(
+        x="Threshold",
+        y="Value",
+        hue="Metric",
+        col="Dataset",
+        col_wrap=3,
+        kind="line",
+        data=df_melted,
+        marker="o",
+        facet_kws={"sharex": False, "sharey": False},
+    )
 
-# Filter the dataframe to only contain the selected metrics and id_vars
-df = df[selected_metrics + id_vars]
+    for item, ax in g.axes_dict.items():
+        ax.set_title(item)
+    # Adjust layout
+    # g.fig.subplots_adjust(top=0.9)
+    # g.fig.suptitle("Metrics vs Threshold for Different Datasets", fontsize=16)
+    g.savefig(f"figures/pre_llm_perf.png")
 
-# Reshape the dataframe for seaborn
-df_melted = pd.melt(df, id_vars=id_vars, var_name="Metric", value_name="Value")
-
-df_melted = rename_datasets(df_melted, preserve_sampled=False)
-
-# Create the relplot
-g = sns.relplot(
-    x="Threshold",
-    y="Value",
-    hue="Metric",
-    col="Dataset",
-    col_wrap=3,
-    kind="line",
-    data=df_melted,
-    marker="o",
-    facet_kws={"sharex": False, "sharey": False},
-)
-
-
-# Adjust layout
-# g.fig.subplots_adjust(top=0.9)
-# g.fig.suptitle("Metrics vs Threshold for Different Datasets", fontsize=16)
-g.savefig(f"figures/pre_llm_perf.png")
-
-# Iterate over each group and plot the dataset
-for dataset_name, group_data in df_melted.groupby("Dataset"):
-    plot_dataset(group_data, dataset_name)
+    # Iterate over each group and plot the dataset
+    for dataset_name, group_data in df_melted.groupby("Dataset"):
+        plot_dataset(group_data, dataset_name)
