1a195d8a82bf29aae05871b81530f0ffb65e0d4d
moemode <pfeffer.moritz@gmail.com>
2023-11-29 12:00:22 +0100

    Create more pre_llm perf visualizations

--- pre_llm_vis.py
+++ pre_llm_vis.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import seaborn as sns
 import pandas as pd
 
@@ -18,7 +19,7 @@ def setup_plt():
     plt.rc("figure", titlesize=16)
 
 
-def plot_dataset(data, dataset_name):
+def plot_dataset(data, dataset_name, postfix):
     # Create the relplot for the specified dataset
     g = sns.relplot(
         x="Threshold",
@@ -32,39 +33,58 @@ def plot_dataset(data, dataset_name):
     g.fig.subplots_adjust(top=0.92)
     # Set title for the plot
     g.fig.suptitle(f"{dataset_name}")
-    g.savefig(f"figures/pre_llm_perf-{dataset_name}.png")
+    g.savefig(f"figures/pre_llm/pre_llm_perf_{postfix}-{dataset_name}.png")
 
+
+CONFIGURATIONS = {
+    "all-metrics": {
+        "selected_metrics": ["Precision", "Recall", "F1", "Cost Relative"],
+        "postfix": "all",
+    },
+    "no-cost": {
+        "selected_metrics": ["Precision", "Recall", "F1"],
+        "postfix": "nc",
+    },
+    "f1-cost": {
+        "selected_metrics": ["F1", "Cost Relative"],
+        "postfix": "fc",
+    },
+}
 
 if __name__ == "__main__":
-    setup_plt()
-    df = pd.read_csv("eval_writeup/pre_llm_perf.csv")
-    id_vars = ["Dataset", "Threshold"]
-    selected_metrics = ["Precision", "Recall", "F1", "Cost Relative"]
-    # Filter the dataframe to only contain the selected metrics and id_vars
-    df = df[selected_metrics + id_vars]
-    # Reshape the dataframe for seaborn
-    df_melted = pd.melt(df, id_vars=id_vars, var_name="Metric", value_name="Value")
-    df_melted = rename_datasets(df_melted, preserve_sampled=False)
-    # Create the relplot
-    g = sns.relplot(
-        x="Threshold",
-        y="Value",
-        hue="Metric",
-        col="Dataset",
-        col_wrap=3,
-        kind="line",
-        data=df_melted,
-        marker="o",
-        facet_kws={"sharex": False, "sharey": False},
-    )
+    for cfg in CONFIGURATIONS.keys():
+        selected_metrics = CONFIGURATIONS[cfg]["selected_metrics"]
+        postfix = CONFIGURATIONS[cfg]["postfix"]
+        setup_plt()
+        df = pd.read_csv("eval_writeup/pre_llm_perf.csv")
+        id_vars = ["Dataset", "Threshold"]
+        # Filter the dataframe to only contain the selected metrics and id_vars
+        df = df[selected_metrics + id_vars]
+        # Reshape the dataframe for seaborn
+        df_melted = pd.melt(df, id_vars=id_vars, var_name="Metric", value_name="Value")
+        df_melted = rename_datasets(df_melted, preserve_sampled=False)
+        # Create the relplot
+        g = sns.relplot(
+            x="Threshold",
+            y="Value",
+            hue="Metric",
+            col="Dataset",
+            col_wrap=3,
+            kind="line",
+            data=df_melted,
+            marker="o",
+            facet_kws={"sharex": False, "sharey": False},
+        )
 
-    for item, ax in g.axes_dict.items():
-        ax.set_title(item)
-    # Adjust layout
-    # g.fig.subplots_adjust(top=0.9)
-    # g.fig.suptitle("Metrics vs Threshold for Different Datasets", fontsize=16)
-    g.savefig(f"figures/pre_llm_perf.png")
+        for item, ax in g.axes_dict.items():
+            ax.set_title(item)
+        # Adjust layout
+        # g.fig.subplots_adjust(top=0.9)
+        # g.fig.suptitle("Metrics vs Threshold for Different Datasets", fontsize=16)
+        p = Path(f"figures/pre_llm/pre_llm_perf_{postfix}.png")
+        p.parent.mkdir(parents=True, exist_ok=True)
+        g.savefig(f"figures/pre_llm_perf_{postfix}.png")
 
-    # Iterate over each group and plot the dataset
-    for dataset_name, group_data in df_melted.groupby("Dataset"):
-        plot_dataset(group_data, dataset_name)
+        # Iterate over each group and plot the dataset
+        for dataset_name, group_data in df_melted.groupby("Dataset"):
+            plot_dataset(group_data, dataset_name, postfix)
