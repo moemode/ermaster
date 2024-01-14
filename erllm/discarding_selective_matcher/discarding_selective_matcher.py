dc6e2123146edd0b877386bd0b2a0fadeb3ee534
moemode <pfeffer.moritz@gmail.com>
2024-01-14 13:11:10 +0100

    Implement dsm

--- erllm/discarding_selective_matcher/discarding_selective_matcher.py
+++ erllm/discarding_selective_matcher/discarding_selective_matcher.py
@@ -1,24 +1,37 @@
 from pathlib import Path
-from typing import List, Set, Tuple
+from typing import Set, Tuple
 import pandas as pd
 from erllm import RUNS_FOLDER_PATH, SIMILARITIES_FOLDER_PATH
 from erllm.discarding_matcher.discarding_matcher import find_matching_csv
-from erllm.llm_matcher.evalrun import read_run_raw
-
-
-def discarder(
-    fraction: float, runFile: Path, similaritiesFile: Path, sim_function="overlap"
-) -> Tuple[List[Tuple[int, int]], List[Tuple[int, int]]]:
-    pass
+from erllm.llm_matcher.evalrun import CompletedPrompt, read_run_raw
 
 
 def discarding_selective_matcher(
-    fraction: float, runFile: Path, similaritiesFile: Path, sim_function="overlap"
+    discard_fraction: float,
+    label_fraction: float,
+    runFile: Path,
+    similaritiesFile: Path,
+    sim_function="overlap",
 ):
     completions = read_run_raw(runFile)
-    dataset_ids: Set[Tuple[int, int]] = set(completions.keys())
-    sims = get_similarities(dataset_ids, fraction, similaritiesFile)
-    discarder(sims, fraction, sim_function)
+    dataset_pair_ids: Set[Tuple[int, int]] = set(completions.keys())
+    sims = get_similarities(dataset_pair_ids, similaritiesFile)
+    n_discard = int(len(dataset_pair_ids) * discard_fraction)
+    discarded, kept = discarder(sims, n_discard, sim_function)
+    discarded_pairs = set(zip(discarded["table1.id"], discarded["table2.id"]))
+    kept_pairs = set(zip(kept["table1.id"], kept["table2.id"]))
+    kept_completions = {
+        pair_id: cp for pair_id, cp in completions.items() if pair_id in kept_pairs
+    }
+    predictions, llm_pairs, human_label_pairs = selective_matcher(
+        label_fraction, kept_completions
+    )
+    predictions.update({pair_id: False for pair_id in discarded_pairs})
+    assert len(predictions) == len(dataset_pair_ids)
+    assert len(discarded_pairs) + len(llm_pairs) + len(human_label_pairs) == len(
+        dataset_pair_ids
+    )
+    return predictions, discarded_pairs, llm_pairs, human_label_pairs
 
 
 def get_similarities(
@@ -42,16 +55,31 @@ def get_similarities(
     return similarities
 
 
-def discarder(similarities: pd.DataFrame, fraction: float, sim_function="overlap"):
+def discarder(similarities: pd.DataFrame, n_discard: int, sim_function="overlap"):
     if not sim_function in similarities.columns:
         raise ValueError(
             f"Similarity file does not have a column named {sim_function}."
         )
     similarities_sorted = similarities.sort_values(by=[sim_function])
-    n_discard = int(len(similarities) * fraction)
     discarded_rows = similarities_sorted.head(n_discard)
     kept_rows = similarities_sorted.tail(len(similarities) - n_discard)
     return discarded_rows, kept_rows
+
+
+def selective_matcher(
+    label_fraction: float, completions: dict[Tuple[int, int], CompletedPrompt]
+) -> Tuple[dict[Tuple[int, int], bool], Set[Tuple[int, int]], Set[Tuple[int, int]]]:
+    predictions: dict[Tuple[int, int], bool] = {
+        id_pair: cp.prediction for id_pair, cp in completions.items()
+    }
+    n_label = int(len(completions) * label_fraction)
+    # get the n_label id pairs with smallest confidence
+    sorted_completions = sorted(completions.values(), key=lambda cp: cp.probability)
+    least_confident_ids = set((cp.id0, cp.id1) for cp in sorted_completions[:n_label])
+    llm_ids = set(completions.keys()) - least_confident_ids
+    for id_pair in least_confident_ids:
+        predictions[id_pair] = completions[id_pair].truth
+    return predictions, llm_ids, least_confident_ids
 
 
 if __name__ == "__main__":
@@ -73,4 +101,4 @@ if __name__ == "__main__":
             raise ValueError(
                 f"No matching similarity file in {CONFIGURATIONS['base']['similarities']} found for {path}"
             )
-        print(discarding_selective_matcher(0.3, path, simPath))
+        print(discarding_selective_matcher(0.3, 0.1, path, simPath))
