4f1cee08f62a72d61031ca754bd9b8cb04df3154
moemode <pfeffer.moritz@gmail.com>
2024-02-03 13:34:07 +0100

    Complete DBpedia to ditto conversion

--- erllm/dataset/dbpedia/to_ditto.py
+++ erllm/dataset/dbpedia/to_ditto.py
@@ -28,8 +28,8 @@ def to_ditto(
             file.write(line)
 
 
-def entities_from_dbpedia_csv(
-    dbpedia_csv: Path,
+def entities_from_dbpedia_df(
+    df: pd.DataFrame,
 ) -> Iterable[tuple[Entity, Entity, int]]:
     """
     Extracts entities from a DBpedia CSV file.
@@ -40,7 +40,6 @@ def entities_from_dbpedia_csv(
     Returns:
         Iterable[tuple[Entity, Entity, int]]: A list of tuples containing the entities and their labels.
     """
-    df = pd.read_csv(dbpedia_csv)
     pair_ids = [
         (row["table1.id"], row["table2.id"], row["label"]) for _, row in df.iterrows()
     ]
@@ -56,9 +55,18 @@ def entities_from_dbpedia_csv(
     return pair_entities
 
 
-def dbpedia_to_test_train_ditto(
+def sample_df(df: pd.DataFrame, n_pos: int, n_neg: int, seed: int) -> pd.DataFrame:
+    # sample N_train_pos rows with label 1 and N_train_neg rows with label 0
+    pos = df[df["label"] == 1].sample(n_pos, random_state=seed)
+    # Sample N_train_neg rows with label 0
+    neg = df[df["label"] == 0].sample(n_neg, random_state=seed)
+    # Concatenate the positive and negative samples to create the final training set
+    return pd.concat([pos, neg])
+
+
+def dbpedia_to_train_valid_test(
     dbpedia_test_csv: Path, dbpedia_reserve_csv: Path, label_fraction: float
-):
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """
     Extracts entities from a DBpedia CSV file.
 
@@ -69,41 +77,30 @@ def dbpedia_to_test_train_ditto(
         Iterable[tuple[Entity, Entity, int]]: A list of tuples containing the entities and their labels.
     """
     seed = 123
-    df_test_train = pd.read_csv(dbpedia_test_csv)
-    df_valid = pd.read_csv(dbpedia_reserve_csv)
-    df_test_train_ids = [
-        (row["table1.id"], row["table2.id"]) for _, row in df_test_train.iterrows()
-    ]
+    df_test = pd.read_csv(dbpedia_test_csv)
+    df_reserve = pd.read_csv(dbpedia_reserve_csv)
     # remove all rows with ids in df_test_train_ids from df_valid
-    df_valid = df_valid[
-        ~df_valid[["table1.id", "table2.id"]]
+    df_test_train_ids = [
+        (row["table1.id"], row["table2.id"]) for _, row in df_test.iterrows()
+    ]
+    df_reserve = df_reserve[
+        ~df_reserve[["table1.id", "table2.id"]]
         .apply(tuple, axis=1)
         .isin(df_test_train_ids)
     ]
     N, N_pos, N_neg = (
-        len(df_test_train),
-        sum(df_test_train["label"]),
-        len(df_test_train) - sum(df_test_train["label"]),
+        len(df_test),
+        sum(df_test["label"]),
+        len(df_test) - sum(df_test["label"]),
     )
-    N_train, N_train_pos = (label_fraction * N, round(label_fraction * N_pos))
+    N_train, N_train_pos = (
+        int(round(label_fraction * N)),
+        int(round(label_fraction * N_pos)),
+    )
     N_train_neg = N_train - N_train_pos
-
-    # sample N_train_pos rows with label 1 and N_train_neg rows with label 0
-    train_ids = df_test_train[df_test_train["label"] == 1].sample(N_train_pos, seed)
-    train_ids = train_ids.append(
-        df_test_train[df_test_train["label"] == 0].sample(N_train_neg, seed)
-    )
-
-    test_entities = []
-    for id0, id1, label in test_ids:
-        test_entities.append(
-            (
-                get_entity_by_id(id0, "dbpedia0"),
-                get_entity_by_id(id1, "dbpedia1"),
-                label,
-            )
-        )
-    return test_entities
+    df_train = sample_df(df_test, N_train_pos, N_train_neg, seed)
+    df_valid = sample_df(df_reserve, N_train_pos, N_train_neg, seed)
+    return df_train, df_valid, df_test
 
 
 if __name__ == "__main__":
@@ -115,12 +112,19 @@ if __name__ == "__main__":
     dbpedia10k_csv = (
         DATA_FOLDER_PATH / "benchmark_datasets/existingDatasets/dbpedia10k/train.csv"
     )
-    ditto_file = Path(
-        "/home/v/coding/ermaster/data/benchmark_datasets/ditto/dbpedia10k_1250/test.txt"
+    dbpedia_ditto_folder = Path(
+        "/home/v/coding/ermaster/data/benchmark_datasets/ditto/dbpedia"
     )
-    dbpedia_to_test_train_ditto(dbpedia1250_csv, dbpedia10k_csv, label_fraction)
+    dbpedia_ditto_folder.mkdir(parents=True, exist_ok=True)
+    dfs = dbpedia_to_train_valid_test(dbpedia1250_csv, dbpedia10k_csv, label_fraction)
+    for df, stem in zip(dfs, ("train", "valid", "test")):
+        entities = entities_from_dbpedia_df(df)
+        ditto_file = dbpedia_ditto_folder / f"{stem}.txt"
+        to_ditto(entities, ditto_file)
+
     """
-    labeled_pairs = list(entities_from_dbpedia_csv(dbpedia1250_csv))
+    df = pd.read_csv(dbpedia1250_csv)
+    labeled_pairs = list(entities_from_dbpedia_csv(df))
     ditto_file.parent.mkdir(parents=True, exist_ok=True)
     to_ditto(labeled_pairs, ditto_file)
     """
