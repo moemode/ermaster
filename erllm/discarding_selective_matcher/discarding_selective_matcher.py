1024d4ef5d9b8fc9e7d80dea645d2f85efc4d248
moemode <pfeffer.moritz@gmail.com>
2024-02-14 17:36:33 +0100

    Document dsm.py

--- erllm/discarding_selective_matcher/discarding_selective_matcher.py
+++ erllm/discarding_selective_matcher/discarding_selective_matcher.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Dict, Set, Tuple
+from typing import Any, Dict, Set, Tuple
 import pandas as pd
 from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
 from erllm import RUNS_FOLDER_PATH, SIMILARITIES_FOLDER_PATH
@@ -22,6 +22,25 @@ def discarding_selective_matcher(
     float,
     float,
 ]:
+    """
+    Runs the discarding selective matcher and returns its predictions and intermediate data like the identifiers of discarded pairs.
+
+    Args:
+        discard_fraction (float): The fraction of dataset pairs to discard.
+        label_fraction (float): The fraction of dataset pairs to label.
+        runFile (Path): The path to the run file containing completions for dataset pairs.
+        similaritiesFile (Path): The path to the file containing pairwise similarities between dataset pairs.
+        sim_function (str, optional): The similarity function to use. Defaults to "overlap".
+
+    Returns:
+        Tuple[Dict[Tuple[int, int], bool], Set[Tuple[int, int]], Set[Tuple[int, int]], Set[Tuple[int, int]], float, float]:
+            - predictions (Dict[Tuple[int, int], bool]): A dictionary mapping dataset pair IDs to their predicted labels.
+            - discarded_pairs (Set[Tuple[int, int]]): A set of dataset pairs that were discarded.
+            - llm_pairs (Set[Tuple[int, int]]): A set of dataset pairs that were labeled by the selective matcher.
+            - human_label_pairs (Set[Tuple[int, int]]): A set of dataset pairs that were labeled by human experts.
+            - discarder_threshold (float): The threshold value used by the discarding algorithm.
+            - confidence_threshold (float): The threshold value used by the selective matcher.
+    """
     if discard_fraction + label_fraction > 1:
         raise ValueError(
             "The sum of discard_fraction and label_fraction must not be greater than 1."
@@ -57,7 +76,20 @@ def discarding_selective_matcher(
 def get_similarities(
     dataset_ids: set[Tuple[int, int]],
     similarities_file: Path,
-):
+) -> pd.DataFrame:
+    """
+    Retrieves the similarities between pairs of dataset IDs from a CSV file.
+
+    Args:
+        dataset_ids (set[Tuple[int, int]]): A set of tuples representing pairs of dataset IDs.
+        similarities_file (Path): The path to the CSV file containing the similarities.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the similarities between the specified dataset ID pairs.
+
+    Raises:
+        ValueError: If the similarity file does not contain values for some pairs or if the number of rows in the similarity file does not match the expected number of pairs.
+    """
     similarities = pd.read_csv(similarities_file)
     sim_pairs = set(zip(similarities["table1.id"], similarities["table2.id"]))
     if not set(dataset_ids).issubset(sim_pairs):
@@ -74,7 +106,20 @@ def get_similarities(
     return similarities
 
 
-def discarder(similarities: pd.DataFrame, n_discard: int, sim_function="overlap"):
+def discarder(
+    similarities: pd.DataFrame, n_discard: int, sim_function="overlap"
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Discards a specified number of rows from a DataFrame based on a similarity function.
+
+    Args:
+        similarities (pd.DataFrame): The DataFrame containing the similarity values.
+        n_discard (int): The number of rows to discard.
+        sim_function (str, optional): The name of the similarity function column. Defaults to "overlap".
+
+    Returns:
+        tuple[pd.DataFrame, pd.DataFrame]: A tuple containing two DataFrames: the discarded rows and the kept rows.
+    """
     if not sim_function in similarities.columns:
         raise ValueError(
             f"Similarity file does not have a column named {sim_function}."
@@ -90,6 +135,22 @@ def selective_matcher(
 ) -> Tuple[
     dict[Tuple[int, int], bool], Set[Tuple[int, int]], Set[Tuple[int, int]], float
 ]:
+    """
+    Returns predictions based on completions but sets the n_label least confident predictions to the true label.
+    This simulates perfect manual labeling.
+
+    Args:
+        n_label (int): The number of labels to match.
+        completions (dict[Tuple[int, int], CompletedPrompt]): A dictionary of completions.
+
+    Returns:
+        Tuple[dict[Tuple[int, int], bool], Set[Tuple[int, int]], Set[Tuple[int, int]], float]:
+            - predictions: A dictionary of label predictions.
+            - llm_ids: A set of all label IDs.
+            - least_confident_ids: A set of the least confident label IDs.
+            - threshold: The confidence score threshold.
+
+    """
     predictions: dict[Tuple[int, int], bool] = {
         id_pair: cp.prediction for id_pair, cp in completions.items()
     }
@@ -103,17 +164,44 @@ def selective_matcher(
     return predictions, llm_ids, least_confident_ids, threshold
 
 
-def llm_cost_duration(pairs, completions, outtokens_per_prompt, model):
+def llm_cost_duration(
+    pairs: Set[tuple[int, int]],
+    completions: dict[Tuple[int, int], CompletedPrompt],
+    outtokens_per_prompt: int,
+    model: str,
+) -> tuple[float, float]:
+    """
+    Calculate the total cost and duration for obtaining the completions.
+    Args:
+        pairs (Set[tuple[int, int]]): A set of pairs representing the indices of prompts.
+        completions (dict[Tuple[int, int], CompletedPrompt]): A dictionary mapping pairs to CompletedPrompt objects.
+        outtokens_per_prompt (int): The number of output tokens per prompt.
+        model (str): The name of the language model.
+
+    Returns:
+        tuple[float, float]: A tuple containing the cost and duration of running the language model matcher.
+    """
     prompts = [completions[p].prompt_string for p in pairs]
-    # cost of running basic matcher without discarder
     cost = str_cost(prompts, outtokens_per_prompt, model)
     duration = sum([completions[p].duration for p in pairs])
     return cost, duration
 
 
-def discarder_duration(pairs, simfile, sim_function):
+def discarder_duration(
+    pairs: Set[tuple[int, int]], simfile: Path, sim_function: str
+) -> float:
+    """
+    Calculate the duration the discarder alone takes on deciding on the pairs.
+
+    Args:
+        pairs (Set[tuple[int, int]]): A set of pairs representing table IDs.
+        simfile (Path): The path to the similarity file.
+        sim_function (str): The name of the similarity function.
+
+    Returns:
+        float: The total duration of the discarding selective matcher.
+    """
     similarities = pd.read_csv(simfile)
-    # cost of running basic matcher without discarder
     sims_for_pairs = similarities[
         similarities.apply(
             lambda row: (row["table1.id"], row["table2.id"]) in pairs, axis=1
@@ -131,7 +219,30 @@ def intermediate_stats(
     human_label_pairs: Set[tuple[int, int]],
     simfile: Path,
     sim_function: str,
-):
+) -> dict[str, float | int]:
+    """
+    Calculate intermediate statistics for the discarding selective matcher based on the output of
+    discarding_selective_matcher function.
+
+    Args:
+        completions (Dict[tuple[int, int], CompletedPrompt]): A dictionary of completion pairs.
+        discarded_pairs (Set[tuple[int, int]]): A set of discarded completion pairs.
+        llm_pairs (Set[tuple[int, int]]): A set of LLM completion pairs.
+        human_label_pairs (Set[tuple[int, int]]): A set of completion pairs with human labels.
+        simfile (Path): The path to the similarity file.
+        sim_function (str): The similarity function to use.
+
+    Returns:
+        dict: A dictionary containing the intermediate statistics:
+            - "Discarded": The number of discarded pairs.
+            - "Discarded FN": The number of discarded pairs with a true truth value.
+            - "Discarder Duration": The duration of the discarder.
+            - "LLM All Cost": The cost of LLM for all pairs.
+            - "LLM All Duration": The duration of LLM for all pairs.
+            - "LLM Cost": The cost of LLM for LLM pairs.
+            - "LLM Duration": The duration of LLM for LLM pairs.
+            - "Manual": The number of pairs with human labels.
+    """
     all_pairs = set(completions.keys())
     n_discarded = len(discarded_pairs)
     discarder_dur = discarder_duration(discarded_pairs, simfile, sim_function)
@@ -161,7 +272,27 @@ def eval_discarding_selective_matcher(
     runfile: Path,
     simfile: Path,
     sim_function="overlap",
-) -> dict:
+) -> Dict[str, Any]:
+    """
+    Fully evaluate the discarding selective matcher.
+
+    Args:
+        discard_fraction (float): The fraction of pairs to discard.
+        label_fraction (float): The fraction of pairs to label.
+        runfile (Path): The path to the runfile.
+        simfile (Path): The path to the similarity file.
+        sim_function (str, optional): The similarity function to use. Defaults to "overlap".
+
+    Returns:
+        dict: A dictionary containing the evaluation metrics.
+            - "N" (int): The total number of completions.
+            - "Discarder Threshold" (float): The discarder threshold.
+            - "Confidence Threshold" (float): The confidence threshold.
+            - "Accuracy" (float): The accuracy score.
+            - "Precision" (float): The precision score.
+            - "Recall" (float): The recall score.
+            - "F1" (float): The F1 score.
+    """
     completions = read_run_raw(runfile)
     (
         predictions,
@@ -197,16 +328,6 @@ def eval_discarding_selective_matcher(
         "Recall": recall_score(truths_list, predictions_list),
         "F1": f1_score(truths_list, predictions_list),
     }
-    """
-    "Cost": cost,
-    "Cost Relative": cost / cost_llm_matcher,
-    "Duration": duration,
-    "Duration Relative": duration / duration_llm_matcher,
-    "LLM Duration": llm_duration,
-    "Sim Duration": sim_duration,
-    "Sim Function": sim_function,
-    "LLM Matcher Duration": duration_llm_matcher,
-    """
 
 
 if __name__ == "__main__":
