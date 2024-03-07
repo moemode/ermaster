67eb88783d4068aaec7b18e8ac5bd4bc7c842472
moemode <pfeffer.moritz@gmail.com>
2024-03-07 13:07:04 +0100

    Create table with actual and est performance metrics

--- eval_writeup/estimated_performance.py
+++ eval_writeup/estimated_performance.py
@@ -71,3 +71,31 @@ result_df.to_latex(
 # Display the updated DataFrame
 print(result_df)
 # df.to_csv("eval_writeup/base_selected.csv", index=False)
+
+df.set_index("Dataset", inplace=True)
+# Create a multi-index for the new DataFrame with switched levels
+multi_index = pd.MultiIndex.from_product(
+    [["Accuracy", "Precision", "Recall", "F1"], ["Actual", "Est."]],
+    # names=["Metric", "Value"],
+)
+
+# Create a new DataFrame with the multi-index
+new_df = pd.DataFrame(index=df.index, columns=multi_index)
+
+# Fill in the values for the new DataFrame
+for metric in ["Accuracy", "Precision", "Recall", "F1"]:
+    new_df[(metric, "Actual")] = df[metric]
+    new_df[(metric, "Est.")] = df[f"EST_{metric}"]
+
+print(new_df)
+new_df.index.name = ""
+s = new_df.style
+s.format(precision=3)
+latex_table = s.to_latex(
+    "eval_writeup/estimated_performance_abs.tex",
+    # column_format="|l|cc|cc|cc|",
+    hrules=True,
+    position_float="centering",
+    multicol_align="c",
+    caption="Comparison of actual and estimated performance of LLM Matcher (gpt-3.5-turbo-instruct) using base prompt prefix.",
+)
