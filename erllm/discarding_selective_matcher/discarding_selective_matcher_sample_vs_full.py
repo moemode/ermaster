9126e947a0c7981d483a805129a8abf8fca0700b
moemode <pfeffer.moritz@gmail.com>
2024-03-19 14:04:30 +0100

    Start on eval of dbpedia full vs sample

---
+++ erllm/discarding_selective_matcher/discarding_selective_matcher_sample_vs_full.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from erllm import EVAL_FOLDER_PATH
+
+
+CONFIGURATIONS = {
+    "dbpedia": {
+        "full_folder": EVAL_FOLDER_PATH
+        / "discarding_selective_matcher"
+        / "dbpedia_full",
+        "sampled_folder": EVAL_FOLDER_PATH / "discarding_selective_matcher" / "grid",
+        "dsm_configs": [(0.0, 0.0), (0.8, 0.0), (0.0, 0.15), (0.5, 0.15)],
+    },
+}
+
+cfg = CONFIGURATIONS["dbpedia"]
+full_df = pd.read_csv(cfg["full_folder"] / "result.csv")
+sample_df = pd.read_csv(cfg["sampled_folder"] / "result.csv")
+
+# get all datasets in full_df
+full_datasets = full_df["Dataset"].unique()
+sampled_datasets = sample_df["Dataset"].unique()
+keep_sampled_datasets = filter(
+    lambda ds: any([full_ds in ds for full_ds in full_datasets]), sampled_datasets
+)
+# only keep rows in sample_df where the Dataset column is contained in any of the strings in full_datasets
+sample_df = sample_df[sample_df["Dataset"].isin(keep_sampled_datasets)]
+# only keep rows where Discard Fraction, Label Fraction is in cfg["dsm_configs"]
+full_df = full_df[
+    full_df[["Discard Fraction", "Label Fraction"]].apply(
+        lambda row: tuple(row) in cfg["dsm_configs"], axis=1
+    )
+]
+sample_df = sample_df[
+    sample_df[["Discard Fraction", "Label Fraction"]].apply(
+        lambda row: tuple(row) in cfg["dsm_configs"], axis=1
+    )
+]
+# full_result keep Dataset , Discard Fraction, Label Fraction, Recall, Precision, F1
+full_result = full_df[
+    ["Dataset", "Discard Fraction", "Label Fraction", "Recall", "Precision", "F1"]
+]
+sample_result = sample_df[
+    ["Dataset", "Discard Fraction", "Label Fraction", "Recall", "Precision", "F1"]
+]
+# combine the two df into one
+result = pd.concat([full_result, sample_result])
+result = result.set_index(["Dataset", "Discard Fraction", "Label Fraction"])
+print(result)
