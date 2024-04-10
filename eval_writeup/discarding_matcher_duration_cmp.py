2372b174d25ac61516168a538a2d3cbde7f37cfa
moemode <pfeffer.moritz@gmail.com>
2024-04-10 17:19:00 +0200

    move oneoffs to trash

--- eval_writeup/discarding_matcher_duration_cmp.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("eval_writeup/discarding_matcher_duration_cmp.csv")
-df.to_latex(
-    "eval_writeup/discarding_matcher_duration_cmp.ltx", escape=True, index=False
-)
